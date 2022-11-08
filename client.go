3456c0be78d4979a7965a9bc1b50dfd0b92a57e2
Lync <golangaddict@gmail.com>
2022-11-08 18:59:48 -0500

    upd

--- client.go
+++ client.go
@@ -4,6 +4,16 @@ type Client struct {
 	api *apiClient
 }
 
+func New(key string, sandbox bool) *Client {
+	url := "https://api.coingate.com/v2"
+	if sandbox {
+		url = "https://sandbox-api.coingate.com/v2"
+	}
+	return &Client{
+		api: newAPIClient(url, key),
+	}
+}
+
 func (c *Client) CreateOrder(params *CreateOrderParams) (res *CreateOrder, err error) {
 	return res, c.api.send("/orders", "POST", params, res)
 }
